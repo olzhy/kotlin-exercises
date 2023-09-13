45ef16bf58e18f17b51c4dbd4cdffd293501abb5
LeiLei Yan <olzhy@qq.com>
2023-09-13 10:21:36 +0800

    spring boot restful service demo

--- spring-boot-restful-service-demo/src/main/kotlin/com/example/demo/service/UserService.kt
+++ spring-boot-restful-service-demo/src/main/kotlin/com/example/demo/service/UserService.kt
@@ -5,9 +5,13 @@ import com.example.demo.model.User
 interface UserService {
 
     fun listAll(): List<User>
+
     fun getById(id: Long): User?
+
     fun update(user: User)
+
     fun save(user: User)
+
     fun deleteById(id: Long)
 
 }
