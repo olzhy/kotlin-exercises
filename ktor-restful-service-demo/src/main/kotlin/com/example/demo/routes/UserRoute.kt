e491c01b906cc8c64c81d4afaf2f59aaef90b7c4
LeiLei Yan <olzhy@qq.com>
2023-09-30 10:37:10 +0800

    ktor restful service demo

--- ktor-restful-service-demo/src/main/kotlin/com/example/demo/routes/UserRoute.kt
+++ ktor-restful-service-demo/src/main/kotlin/com/example/demo/routes/UserRoute.kt
@@ -1,9 +1,11 @@
 package com.example.demo.routes
 
 import com.example.demo.conf.kodein
+import com.example.demo.model.User
 import com.example.demo.service.UserService
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.kodein.di.instance
@@ -22,7 +24,7 @@ fun Route.userRouting() {
         get(Regex("/(?<id>\\d+)")) {
             val id = call.parameters["id"]!!.toLong()
 
-            val user = userService.getById(id) ?: call.respondText(
+            val user = userService.getById(id) ?: return@get call.respondText(
                     "user not exists",
                     status = HttpStatusCode.NotFound
             )
@@ -31,17 +33,40 @@ fun Route.userRouting() {
 
         // update
         patch {
+            val user = call.receive<User>()
+            userService.getById(user.id) ?: return@patch call.respondText(
+                    "user not exists",
+                    status = HttpStatusCode.NotFound
+            )
 
+            userService.update(user)
+            call.respond(HttpStatusCode.NoContent)
         }
 
         // save
         post {
+            val user = call.receive<User>()
+            userService.getById(user.id)?.let {
+                return@post call.respondText(
+                        "user already exists",
+                        status = HttpStatusCode.NotFound
+                )
+            }
 
+            userService.save(user)
+            call.respond(HttpStatusCode.Created)
         }
 
         // delete by id
         delete(Regex("/(?<id>\\d+)")) {
+            val id = call.parameters["id"]!!.toLong()
+            userService.getById(id) ?: return@delete call.respondText(
+                    "user not exists",
+                    status = HttpStatusCode.NotFound
+            )
 
+            userService.deleteById(id)
+            call.respond(HttpStatusCode.NoContent)
         }
     }
 }
