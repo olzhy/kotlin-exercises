a7aa72f0d8725838aac2410504d41cb07a380830
LeiLei Yan <olzhy@qq.com>
2023-10-01 10:05:38 +0800

    ktor restful service demo

--- ktor-restful-service-demo/build.gradle.kts
+++ ktor-restful-service-demo/build.gradle.kts
@@ -12,12 +12,13 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-serialization-jackson-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("org.kodein.di:kodein-di-jvm:7.20.2")
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-jackson")
+    implementation("org.kodein.di:kodein-di:7.20.2")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
 
-    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.10")
 }
